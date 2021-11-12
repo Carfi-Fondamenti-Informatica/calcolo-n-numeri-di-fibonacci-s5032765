eeb23f74b3603ab101f49c075126d47cedfd65d6
s5032765 <91558903+s5032765@users.noreply.github.com>
2021-11-12 09:07:58 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
-   return 0;
+
+    int n;
+    cin >> n;
+    int k = 1;
+    int m = 1;
+    int l;
+    if(n<2)
+        cout<<"errore";
+    else {
+        cout << k << endl << m << endl;
+        for (int i = 0; i < n - 2; i++) {
+            l = k + m;
+            cout << l << endl;
+            m = k;
+            k = l;
+        }
+    }
+
+
+    return 0;
 }
